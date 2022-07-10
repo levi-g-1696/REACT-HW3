a3255b70cd343941b1babf3fdd03a4cbd408556a
lg1696@gmail.com <lg1696@gmail.com>
2022-07-10 17:31:30 +0300

    1/2 WAY

---
+++ src/urlInputList.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { findAllInRenderedTree } from "react-dom/test-utils";
+import UrlListElement from "./urlListElementComp";
+// <ToggleButton togggleText="ON"  />
+// <ToggleButtonS count:10  />
+export default class UrlInputList extends React.Component {
+    constructor(props) {
+        super(props);
+    let urlLineVal=""
+        this.state = {urllist: [],urlLine:urlLineVal};
+        
+       
+     
+  }
+
+//Add Item
+onAddItem = () => {
+    let newline = this.state.urlLine
+    this.setState({ urllist: [...this.state.urllist,newline] });
+    this.setState({urlLine:""})
+   
+};
+
+//Remove Last Item
+onSortAsc = () => {
+  
+  let arr= this.state.urllist
+  this.setState({urllist:arr.sort()})
+  
+};
+onSortDesc = () => {
+  let arr= this.state.urllist
+  arr = arr.sort()
+  arr = arr.reverse()
+  this.setState({urllist:arr})
+  
+};
+RemoveFromList=(i)=>{
+  
+  let arr=[]
+  arr= [...this.state.urllist]
+  arr.splice(i,1)
+  this.setState({urllist:arr})
+  alert(arr)
+  
+
+}
+RemoveFromList1(indx)
+{
+  let arr=[]
+  arr= [...this.state.urllist]
+  arr.splice(indx,1)
+  this.setState({urllist:[...arr]})
+ // this.setState({urllist:arr})
+//  alert(arr)
+  arr= [...this.state.urllist]
+  alert(arr)
+ // alert(this.state.urllist)
+  
+ }
+  render() {
+    let resarr=[]
+    for (let i=0 ; i< this.state.urllist.length; i++  )
+    { let elem = this.state.urllist[i]
+      resarr.push(<UrlListElement text={elem} key={i}  onRemove={this.RemoveFromList} />)
+    }
+   // this.state.urllist.forEach((elem)=>{resarr.push(<UrlListElement text={elem} key={elem} onRemove={this.RemoveFromList} />)})
+    
+    return (
+ 
+      <div>
+        <form>
+          <label>Enter url {" "}
+            <input
+              type="text"
+              value={this.state.urlLine}
+              size={80}
+              onChange={(e) => this.setState({ urlLine: e.target.value })}
+
+            />
+          </label>{" "}
+        </form>
+        <br></br>
+     
+         <button type="button" onClick={this.onAddItem}>Add Item</button>&nbsp;
+         <button type="button" onClick={this.onSortAsc}>Sort asc</button>&nbsp;
+         <button type="button" onClick={this.onSortDesc}>Sort desc</button>&nbsp;
+         <br></br>
+        <hr></hr>
+        {/* <ul>  
+          {this.state.urllist.map(item => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul> */}
+        {resarr}
+
+
+      
+      </div>)}
+    
+}
